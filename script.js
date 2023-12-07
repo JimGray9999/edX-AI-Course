6d77aae99396008bda1adbb004bb8fc9a1f56190
Jim Gray <jgray8689@gmail.com>
2023-12-07 00:30:54 -0500

    complete tutorial

--- script.js
+++ script.js
@@ -4,34 +4,41 @@ require('dotenv').config();
 const { OpenAI } = require('langchain/llms/openai');
 const inquirer = require('inquirer');
 const { PromptTemplate } = require("langchain/prompts");
+const { StructuredOutputParser } = require("langchain/output_parsers");
+
+// With a `StructuredOutputParser` we can define a schema for the output.
+const parser = StructuredOutputParser.fromNamesAndDescriptions({
+  code: "Javascript code that answers the user's question",
+  explanation: "detailed explanation of the example code provided",
+});
+
+const formatInstructions = parser.getFormatInstructions();
 
 // instantiate a new model
 const model = new OpenAI({ 
-    openAIApiKey: process.env.OPENAI_API_KEY, 
-    temperature: 0,
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    temperature: 1,
     model: 'gpt-3.5-turbo'
   });
 
-// tests the output of the model
-// console.log(model);
-
-  // promptFunc sends the imput to the model
+// promptFunc sends the imput to the model
 const promptFunc = async (input) => {
 try {
     const res = await model.call(input);
 
     // Instantiation of a new object called "prompt" using the "PromptTemplate" class
     const prompt = new PromptTemplate({
-      template: "You are a javascript expert and will answer the user's coding questions thoroughly as possible.\n{question}",
+      template: "You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{format_instructions}\n{question}",
       inputVariables: ["question"],
+      partialVariables: { format_instructions: formatInstructions }
     });
 
     const promptInput = await prompt.format({
       question: input
     });
     
+    console.log(await parser.parse(res));
 
-    console.log(res);
 }
 catch (err) {
     console.error(err);
